b208aded3341f04cba25d071664dd21d057dda6e
CrazyBoyOO1 <poksiwakorn@hotmail.com>
2025-01-14 19:37:20 +0700

    Init TicTacToe

---
+++ flutter_tictactoe/android/app/src/main/kotlin/com/example/flutter_tictactoe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_tictactoe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- fluttertictactoe/android/app/src/main/kotlin/com/example/fluttertictactoe/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.fluttertictactoe
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
